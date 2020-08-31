6e90a98162e56473fe2f5a8a9fc03634eb1f1211
Siva Reddybathula <siva.reddybathula@gmail.com>
2020-08-31 12:31:55 -0700

    Create 1.rb
    Optimal solution with only one iteration

---
+++ 1.rb
@@ -0,0 +1,36 @@
+"""
+Given an array of integers nums and and integer target, return the indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1]
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+"""
+
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+   hash = Hash.new
+   nums.each_with_index do |num, index|
+      find = target - num
+      return [hash[find], index] if hash[find]
+      hash[num] = index 
+   end
+end
